4f20905ec71b8b51e5adb991aa975669f27ba4b1
toda <toda@sidefeed.com>
2020-03-27 09:00:30 +0900

    Initial commit

---
+++ Example/podTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  podTest
+//
+//  Created by taktod on 03/27/2020.
+//  Copyright (c) 2020 taktod. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/podTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  podTest
+//
+//  Created by taktod on 03/27/2020.
+//  Copyright (c) 2020 taktod. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
